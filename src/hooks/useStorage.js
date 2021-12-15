758de872072fb9be51a29525d595666042b3a933
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-12-15 12:36:14 +0100

    Update

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -10,16 +10,18 @@ const useStorage = (file) => {
     useEffect(() => {
         const storageRef = projectStorage.ref(file.name);
         storageRef.put(file).on('state_change', (snap) => {
-
+            let percentage = (snao.bytesTransferred / snap.totalBytes) * 100;
+            setprogres(percentage);
+        }, (err) => {
+            seterror(err);
+        }, async () => {
+            const url = await storageRef.getDownloadURL();
+            seturl(url);
         });
 
     }, [file]);
 
-    return (
-        <div>
-
-        </div>
-    )
+    return (progres,url,error);
 }
 
 export default useStorage
