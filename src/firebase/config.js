affa3918cd154154ccf84a2ae756d9bdecbca1aa
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-12-15 12:57:40 +0100

    Update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -17,6 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const projectStorage = firebase.storage();
-const projectFirestore = firebase.firestore();
+const projectStorage = app.storage();
+const projectFirestore = app.firestore();
 export { projectStorage, projectFirestore };
