44fea2d813ce6a01ecf487d8e5ef360100b63d35
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-12-17 23:25:32 +0100

    Update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,29 @@
-// Import the functions you need from the SDKs you need
+/* 
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage"
 import { getFirestore} from 'firebase/firestore/lite';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+const app = initializeApp({
+  apiKey: "AIzaSyDZNp5IjYRE2bZ2-6Q3LAWQTMAoRbtQ0hE",
+  authDomain: "firegram-26ddd.firebaseapp.com",
+  databaseURL: "https://firegram-26ddd-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "firegram-26ddd",
+  storageBucket: "firegram-26ddd.appspot.com",
+  messagingSenderId: "32244388328",
+  appId: "1:32244388328:web:c80d35cb8fa04220a8545d"
+});
+
+
+const projectStorage = getStorage(app);
+const projectFirestore = getFirestore(app);
+console.log(app);
+console.log(projectStorage);
+console.log(projectFirestore);
+export { projectStorage, projectFirestore }; */
+
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDZNp5IjYRE2bZ2-6Q3LAWQTMAoRbtQ0hE",
   authDomain: "firegram-26ddd.firebaseapp.com",
@@ -17,11 +34,9 @@ const firebaseConfig = {
   appId: "1:32244388328:web:c80d35cb8fa04220a8545d"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const projectStorage = getStorage(app);
-const projectFirestore = getFirestore(app);
-console.log(app);
-console.log(projectStorage);
-console.log(projectFirestore);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const projectFirestore = firebaseApp.firestore();
+
+const projectStorage = firebase.storage();
+
 export { projectStorage, projectFirestore };
