adc64b724626bac891009f685695b40b0e8d0d23
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-12-20 22:05:38 +0100

    Update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,4 @@
-/* 
+
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage"
 import { getFirestore} from 'firebase/firestore/lite';
@@ -20,24 +20,4 @@ const projectFirestore = getFirestore(app);
 console.log(app);
 console.log(projectStorage);
 console.log(projectFirestore);
-export { projectStorage, projectFirestore }; */
-
-import firebase from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDZNp5IjYRE2bZ2-6Q3LAWQTMAoRbtQ0hE",
-  authDomain: "firegram-26ddd.firebaseapp.com",
-  databaseURL: "https://firegram-26ddd-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "firegram-26ddd",
-  storageBucket: "firegram-26ddd.appspot.com",
-  messagingSenderId: "32244388328",
-  appId: "1:32244388328:web:c80d35cb8fa04220a8545d"
-};
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const projectFirestore = firebaseApp.firestore();
-
-const projectStorage = firebase.storage();
-console.log(projectStorage);
-console.log(projectFirestore);
-export { projectStorage, projectFirestore };
+export { projectStorage, projectFirestore }; 
